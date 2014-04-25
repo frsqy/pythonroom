37fc6db28a437670fed15bffe1f508a598397ab6
skierdude101 <ryanchen.email@gmail.com>
2014-04-25 16:08:53 -0700

    pythonroom update

--- draw.py
+++ draw.py
@@ -1,1 +1,30 @@
 # author: skierdude101
+length = 100
+angle = 60
+import turtle
+
+jose = turtle.Turtle()
+
+#jose.forward(100)
+#jose.left(90)
+#jose.forward(100)
+#jose.left(90)
+#jose.forward(100)
+#jose.left(90)
+#jose.forward(100)
+
+jose.color("red")
+
+jose.forward(60)
+jose.left(72)
+jose.color("#18C3E6")
+jose.forward(60)
+jose.left(72)
+jose.color("yellow")
+jose.forward(60)
+jose.left(72)
+jose.color("green")
+jose.forward(60)
+jose.left(72)
+jose.color("blue")
+jose.forward(60)
