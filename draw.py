165f35f6eb60aa3cb8174c4ac9bb8f0728d87510
skierdude101 <ryanchen.email@gmail.com>
2014-04-26 07:56:50 -0700

    Cool Pattern

--- draw.py
+++ draw.py
@@ -1,13 +1,14 @@
 # author: skierdude101
 length = 6
 n=13
-numbers = (0,100,5)
-angle = 180-180*(n-2)/n
+numbers = range(1,1000,10)
+#angle = 180-180*(n-2)/n
 spiral=10
 line=1
 #colors = ["red","orange","yellow","green","blue","magenta","red","orange","yellow","green","blue","magenta","red","orange","yellow","green","blue","magenta"]
 #colors = ["red", "orange", "yellow", "green"]
-cats=range(0,n)
+colors = ["red","orange","yellow","green"]
+#cats=range(0,n)
 import turtle
 
 jose = turtle.Turtle()
@@ -20,7 +21,7 @@ jose = turtle.Turtle()
 #jose.left(90)
 #jose.forward(100)
 
-jose.color("red")
+#jose.color("red")
 
 #for color in colors:
 #	jose.color(color)
@@ -29,7 +30,13 @@ jose.color("red")
 #for color in colors:
 #	jose.forward(100)
 #	jose.left(170)
-for cat in cats:
-	jose.forward(50)
-	jose.left(angle)
+#for cat in cats:
+#	jose.forward(50)
+#	jose.left(angle)
+for number in numbers:
+	for color in colors:
+		jose.color(color)
+		jose.forward(number)
+		jose.left(90)
+	jose.left(4)
 	
