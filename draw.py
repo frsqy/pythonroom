3ead471f1c72ad6a18ca0e1e6ec6bc208174aea2
skierdude101 <ryanchen.email@gmail.com>
2014-04-26 07:47:25 -0700

    shapes

--- draw.py
+++ draw.py
@@ -1,10 +1,12 @@
 # author: skierdude101
-length = 30
-n=18
+length = 6
+n=13
+numbers = (0,100,5)
 angle = 180-180*(n-2)/n
 spiral=10
 line=1
-colors = ["red","orange","yellow","green","blue","magenta","red","orange","yellow","green","blue","magenta","red","orange","yellow","green","blue","magenta"]
+#colors = ["red","orange","yellow","green","blue","magenta","red","orange","yellow","green","blue","magenta","red","orange","yellow","green","blue","magenta"]
+#colors = ["red", "orange", "yellow", "green"]
 cats=range(0,n)
 import turtle
 
@@ -20,8 +22,14 @@ jose = turtle.Turtle()
 
 jose.color("red")
 
-for color in colors:
-	jose.color(color)
-	jose.forward(length)
+#for color in colors:
+#	jose.color(color)
+#	jose.forward(length)
+#	jose.left(angle)
+#for color in colors:
+#	jose.forward(100)
+#	jose.left(170)
+for cat in cats:
+	jose.forward(50)
 	jose.left(angle)
 	
