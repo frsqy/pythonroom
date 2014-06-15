a432495be4ec6f895b39459e1094d01bc3181e4e
skierdude101 <ryanchen.email@gmail.com>
2014-06-15 12:07:49 -0700

    trying turtle stuff

--- draw.py
+++ draw.py
@@ -1,16 +1,54 @@
 import turtle
+bob = turtle.Turtle()
+a = turtle.Turtle
+b = turtle.Turtle
+c = turtle.Turtle
+d = turtle.Turtle
+e = turtle.Turtle
+f = turtle.Turtle
+g = turtle.Turtle
+turtles=[bob,a,b,c,d,e,f,g]
+#bob.speed(1)
+#bob.setpos(-200,-200)
+#bob.setpos(-100,100)
+#bob.setpos(-50,50)
+#bob.setpos(-25,25)
+#bob.setpos(-12.5,12.5)
+#bob.setpos(-6.25,6.25)
+#bob.setpos(-3.125,3.125)
 
-new = turtle.Turtle()
-
-numbers=[1,1000]
+numbers= range(1,1000,10)
 colors=["00FFF7","00FF00","FF70D2","FF2424","3124E6"]
 pentagon=72
 pentalength=50
+for turtle in turtles:
+	for number in numbers:
+		for color in colors:
+			bob.color(color)
+			bob.forward(number)
+			bob.left(pentagon)
+			a.color(color)
+			a.forward(number)
+			a.left(pentagon)
+			b.color(color)
+			b.forward(number)
+			b.left(pentagon)
+			c.color(color)
+			c.forward(number)
+			c.left(pentagon)
+			d.color(color)
+			d.forward(number)
+			d.left(pentagon)
+		bob.left(10)
 
-for number in numbers:
-	for color in colors:
-		new.color(color)
-		new.forward(number)
-		new.left(pentagon)
-	new.left(4)
+
+		
+		
+#for number in numbers:
+#	for color in colors:
+#		bob.color(color)
+#		bob.forward(number)
+#		bob.left(number)
+
+	
 	
