3483e2387430301b6808f18e2b19f7522f292a0b
skierdude101 <ryanchen.email@gmail.com>
2014-06-15 11:28:05 -0700

    colorful pentagon

--- draw.py
+++ draw.py
@@ -2,16 +2,11 @@ import turtle
 
 new = turtle.Turtle()
 
+colors=["00FFF7","00FF00","FF70D2","FF2424","3124E6"]
 pentagon=72
 pentalength=50
-
-new.forward(pentalength)
-new.left(pentagon)
-new.forward(pentalength)
-new.left(pentagon)
-new.forward(pentalength)
-new.left(pentagon)
-new.forward(pentalength)
-new.left(pentagon)
-new.forward(pentalength)
-new.left(pentagon)
+for color in colors:
+	new.color(color)
+	new.forward(pentalength)
+	new.left(pentagon)
+	
