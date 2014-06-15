b35d595fdcbdba9f7af91389470bd55947d5e265
skierdude101 <ryanchen.email@gmail.com>
2014-06-15 11:30:55 -0700

    pentagon repeat

--- draw.py
+++ draw.py
@@ -2,11 +2,15 @@ import turtle
 
 new = turtle.Turtle()
 
+numbers=[1,1000,2]
 colors=["00FFF7","00FF00","FF70D2","FF2424","3124E6"]
 pentagon=72
 pentalength=50
-for color in colors:
-	new.color(color)
-	new.forward(pentalength)
-	new.left(pentagon)
+
+for number in numbers:
+	for color in colors:
+		new.color(color)
+		new.forward(pentalength)
+		new.left(pentagon)
+	new.left(4)
 	
