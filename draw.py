0996f93c5b138b53e627bb10d1f369d2993ccc8c
skierdude101 <ryanchen.email@gmail.com>
2014-06-15 11:22:50 -0700

    pentagon

---
+++ draw.py
@@ -0,0 +1,17 @@
+import turtle
+
+new = turtle.Turtle()
+
+pentagon=72
+pentalength=50
+
+new.forward(pentalength)
+new.left(pentagon)
+new.forward(pentalength)
+new.left(pentagon)
+new.forward(pentalength)
+new.left(pentagon)
+new.forward(pentalength)
+new.left(pentagon)
+new.forward(pentalength)
+new.left(pentagon)
