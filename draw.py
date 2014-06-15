d59671faeafb419dab146ff39772984068e637eb
skierdude101 <ryanchen.email@gmail.com>
2014-06-15 11:33:04 -0700

    pythonroom update

--- draw.py
+++ draw.py
@@ -2,7 +2,7 @@ import turtle
 
 new = turtle.Turtle()
 
-numbers=[1,1000,2]
+numbers=[1,1000]
 colors=["00FFF7","00FF00","FF70D2","FF2424","3124E6"]
 pentagon=72
 pentalength=50
@@ -10,7 +10,7 @@ pentalength=50
 for number in numbers:
 	for color in colors:
 		new.color(color)
-		new.forward(pentalength)
+		new.forward(number)
 		new.left(pentagon)
 	new.left(4)
 	
